c815ff6fcf5f94c31dbc9fc046c0712229368741
Dimitri Mestdagh <dim00z@users.noreply.github.com>
2023-09-01 18:14:19 +0200

    Update plugin version number
    from v0.0.5 to v1.0.0

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_RESERVATIONINFO_VERSION', '0.0.5');
+define('PLUGIN_RESERVATIONINFO_VERSION', '1.0.0');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_RESERVATIONINFO_MIN_GLPI', '10.0.0');
